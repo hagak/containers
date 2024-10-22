027bc9d63c1abe2f8f9320e007763a92e5ebd14b
Jeffrey Gordon <jeff@thetank.org>
2024-10-22 18:25:19 -0400

    fix name and version

--- apps/cniplugin/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://github.com/containernetworking/plugins/releases/tag/v1.6.0" | jq --raw-output '. | .tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
