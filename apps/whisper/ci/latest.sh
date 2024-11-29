55873b5363f070045424a09bafa0dc9da807877c
Jeffrey Gordon <jeff@thetank.org>
2024-11-29 16:44:52 -0500

    try this way to build

--- apps/whisper/ci/latest.sh
+++ apps/whisper/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET "https://api.github.com/repos/rhasspy/wyoming-faster-whisper/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-version="2.0.0"
+#version="2.0.0"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
