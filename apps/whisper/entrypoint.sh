b09f6d36aafefdb14ec2352839c146a24c7972c8
Jeffrey Gordon <jeff@thetank.org>
2024-11-30 16:23:40 -0500

    update whisper

--- apps/whisper/entrypoint.sh
+++ apps/whisper/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-#shellcheck disable=SC2086
-
-exec \
-    python3 -m wyoming_faster_whisper \
+python3 -m wyoming_faster_whisper \
     --uri 'tcp://0.0.0.0:10300' \
     --data-dir /data \
+    --device cuda \
     --download-dir /data "$@"
-    --download-dir /data "$@" \
-    --device cuda
